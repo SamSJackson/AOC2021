0df37b16730766383380e1868c3aafb73d9b2629
SamSJackson <samuel.jackson.scotland2@gmail.com>
2021-12-06 23:10:49 +0000

    Day 6

--- Day 6/submarineFish.py
+++ Day 6/submarineFish.py
@@ -1,4 +1,5 @@
 # Finding a way to simulate the number of fish
+from collections import defaultdict
 
 def readDataList(file):
 	f = open(file)
@@ -10,25 +11,28 @@ def readDataList(file):
 # Each day, the internal clock on the fish goes down by one
 # If the fish is at 0, it will reset to 6 and append an 8 onto the list
 
+defaultDictionary = defaultdict(int)
 
-# This could be recursive
-def ageFish(fishList : list, days : int, counter=1):
-	if days == 0:
-		return len(fishList)
-	else:
-		for i, value in enumerate(fishList):
-			if value == 0:
-				fishList[i]=6
-				fishList.append(9)
-			else:
-				fishList[i] -= 1
-		#print(f"After {counter} days: {fishList}")
-		print(f"Day: {counter}")
-		return ageFish(fishList, days-1, counter+1)
-
-dataList = readDataList("inputLanternfish2.txt")
-#numberOfFish = ageFish(dataList, 256)
+dataList = readDataList("inputLanternfish.txt")
 #print(f"{numberOfFish=}")
+
+for number in dataList:
+	if number not in defaultDictionary:
+		defaultDictionary[number] = 0
+	defaultDictionary[number] += 1
+
+for day in range(2):
+	#print(day, len(defaultDictionary))
+	newDefaultDict = defaultdict(int)
+	print(defaultDictionary.items())
+	for k,cnt in defaultDictionary.items():
+		if k == 0:
+			newDefaultDict[6] += cnt
+			newDefaultDict[8] += cnt
+		else:
+			newDefaultDict[k-1] += cnt
+	defaultDictionary = newDefaultDict
+print(sum(defaultDictionary.values()))
 
 
 
